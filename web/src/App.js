8d864ec0a46321a52aa555322e32fc7c157a4ff4
Evencaster <iloveyoucreator@gmail.com>
2021-11-23 21:02:44 +0300

    Frontend added

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './pages/WelcomePage.css';
+import Search from "./components/Search";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+} from "react-router-dom";
+import WelcomePage from "./pages/WelcomePage";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/">
+                    <Route path="/search" element={<SearchPage/>} />
+                    <Route index element={<WelcomePage/>} />
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
