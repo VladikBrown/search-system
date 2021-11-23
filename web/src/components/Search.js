1e45078160ef41803894f7603febd04eff066451
Evencaster <iloveyoucreator@gmail.com>
2021-11-23 21:24:39 +0300

    Search input value fixed to correspond url

--- web/src/components/Search.js
+++ web/src/components/Search.js
@@ -1,15 +1,19 @@
 import './Search.css'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function Search(props) {
-    const {onSearch} = props;
+    const {onSearch, value} = props;
     const [text, setText] = useState('');
+    useEffect(() => value !== '' && setText(value), [value]);
+
     return (
         <div style={{display: 'flex'}}>
             <input
                 type="text"
                 className="Search-input"
-                onChange={e => setText(e.target.value)}/>
+                onChange={e => setText(e.target.value)}
+                value={text}
+            />
             <span className="GO-button" onClick={() => onSearch(text)}>
                 GO
             </span>
