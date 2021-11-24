a7a3d914bfd465b1ac5b9ca540dcd814c0a60a39
Evencaster <iloveyoucreator@gmail.com>
2021-11-24 20:50:46 +0300

    Go button is clicked on enter key

--- web/src/components/Search.js
+++ web/src/components/Search.js
@@ -1,10 +1,11 @@
 import './Search.css'
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 
 export default function Search(props) {
     const {onSearch, value} = props;
     const [text, setText] = useState('');
     useEffect(() => value !== '' && setText(value), [value]);
+    const GoButton = React.createRef();
 
     return (
         <div style={{display: 'flex'}}>
@@ -13,8 +14,13 @@ export default function Search(props) {
                 className="Search-input"
                 onChange={e => setText(e.target.value)}
                 value={text}
+                onKeyUp={(e) => {
+                    if(e.keyCode === 13){
+                        GoButton.current.click();
+                    }
+                }}
             />
-            <span className="GO-button" onClick={() => onSearch(text)}>
+            <span className="GO-button" onClick={() => onSearch(text)} ref={GoButton}>
                 GO
             </span>
         </div>
