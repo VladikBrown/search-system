4e5690800834bed2c0da3366f4cb83ebb0aacfdc
Evencaster <iloveyoucreator@gmail.com>
2021-11-24 23:18:38 +0300

    Header component added

--- web/src/pages/SearchPage.js
+++ web/src/pages/SearchPage.js
@@ -3,17 +3,19 @@ import Search from "../components/Search";
 import Results from "../components/Results";
 import {useEffect, useState} from "react";
 import superagent from "superagent";
-import {useNavigate, useSearchParams} from "react-router-dom";
+import {useSearchParams} from "react-router-dom";
+import Header from "../components/Header";
 
 function SearchPage() {
-    const navigate = useNavigate();
     const [results, setResults] = useState([]);
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();
+
+    const query = searchParams.get('query');
 
     useEffect(() => {
         superagent
             .get('http://localhost:3333/search')
-            .query({query: searchParams.get('query')})
+            .query({query})
             .then(res => {
                 setResults(
                     res.body.Docs
@@ -25,28 +27,11 @@ function SearchPage() {
             .catch(err => {
                 alert(err)
             });
-    }, [searchParams]);
+    }, [query]);
 
     return (
         <div className="SearchPage">
-            <div className="SearchLine">
-                <h1
-                    style={{
-                        fontSize: '40px',
-                        cursor: 'pointer',
-                    }}
-                    onClick={() => navigate('/')}
-                >
-                    SS
-                </h1>
-                <Search
-                    onSearch={async query => {
-                        const newSearchParams = new URLSearchParams({query});
-                        setSearchParams(newSearchParams);
-                    }}
-                    value={searchParams.get('query')}
-                />
-            </div>
+            <Header/>
             <Results items={results}/>
         </div>
     );
